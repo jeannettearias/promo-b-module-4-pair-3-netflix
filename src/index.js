045f457fd6de6c1e5ca0ab967b5fa4c0871a819b
jeannettearias <jeannettearias@icloud.com>
2024-09-17 20:44:50 +0200

    create the server function with data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+//import libraries
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2');
 
 // create and config server
 const server = express();
@@ -14,3 +16,49 @@ server.listen(serverPort, () => {
 
 server.use(express.static('./web'));
 
+// Endpoints
+// server.get('/', (req, res) => {
+//   res.send('Its everything is OK!!');
+// });
+
+// server.get('/', (req, res) => {
+
+//   mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'Passw0rd',
+//     database: 'netflixpro'
+//   })
+//     .then(conn => {
+//       conn.connection();
+//     })
+// })
+//endpoint
+
+server.get('/movies', (req, res) => {
+
+  res.json(data);
+
+})
+
+const data =
+{
+  success: true,
+  movies: [
+    {
+      id: '1',
+      title: 'Gambita de dama',
+      genre: 'Drama',
+      image:
+        '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
+    },
+    {
+      id: '2',
+      title: 'Friends',
+      genre: 'Comedia',
+      image:
+        '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
+    }
+  ]
+};
